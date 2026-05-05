56d9095c37df87fb2e66249d321a9c1cf5ae28ec
John Seagull <149725024+thecreeper3326@users.noreply.github.com>
2026-05-05 19:42:26 -0400

    made optionwidth consistent between modmenu and command entry

--- src/client/java/net/creeperdev/noCombatElytra/client/ModMenu.java
+++ src/client/java/net/creeperdev/noCombatElytra/client/ModMenu.java
@@ -9,7 +9,7 @@ import net.minecraft.network.chat.Component;
 public class ModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return p -> new FigScreen<>(Component.literal(""), 100, FigManager.FIGS, p);
+        return p -> new FigScreen<>(Component.literal(""), 0.5f, FigManager.FIGS, p);
     }
 }
 
