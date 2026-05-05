c9b426536158fa185e9e2d9c85769125a860612d
John Seagull <149725024+thecreeper3326@users.noreply.github.com>
2026-05-04 20:19:27 -0400

    e

---
+++ README.md
@@ -0,0 +1,17 @@
+This branch is for 26.1.x versions.
+
+# No Combat Elytra
+
+Prevents players from using their elytra to flee in the middle of combat in Minecraft.
+
+This server-side mod adds a peice of custom data to every player that tracks how long since they have been attacked by another player. This counter will go up indefinately and will be set to 0 when attacked by another player. If a player's counter is below the configureted cooldown, it will remove any elytra from their chest armor slot and place it in an empty space in their inventory. If there is no space for the elytra, it will be dropped on the ground. Enchantments, damage, and other data will be preserved when this happens.
+
+Configurations:
+- Enable/disable mod: useful for disabling/enabling without reloading server
+- Cooldown: amount of ticks that must pass since being hit by a player before they can use their elytra
+- Enable message: enables an actionbar message showing a custom message that may include the time remaining
+- Message: custom message to show during cooldown
+  - Use `%T` to express amount of ticks left in cooldown
+  - Use `%S` to express amount of seconds left in cooldown (rounded ticks/20)
+
+Configuration powered by FigManager 2.2 embedded
---
+++ src/main/java/net/creeperdev/noCombatElytra/NoCombatElytra.java
@@ -0,0 +1,84 @@
+package net.creeperdev.noCombatElytra;
+
+import net.creeperdev.figManager.FigManager;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerBossEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Unit;
+import net.minecraft.world.BossEvent;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.CustomData;
+
+import javax.xml.crypto.Data;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class NoCombatElytra implements ModInitializer {
+    public static String figManagerName = "no_combat_elytra";
+    public static String projectVersion = "1.0";
+    @Override
+    public void onInitialize() {
+        FigManager e = new FigManager();
+        e.init(figManagerName,projectVersion,Figs.instance);
+
+
+
+        ServerTickEvents.END_SERVER_TICK.register((server) -> {
+            Figs f = (Figs) FigManager.FIGS;
+            for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                int time = 0;
+                CustomData cd = player.get(DataComponents.CUSTOM_DATA);
+                try {
+                    if (cd.copyTag().getInt("time_since_combat").isPresent()) {
+                        time = cd.copyTag().getInt("time_since_combat").get();
+                    }
+                    CompoundTag nbt =new CompoundTag();
+                    nbt.putInt("time_since_combat",time+1);
+                    player.setComponent(DataComponents.CUSTOM_DATA,CustomData.of(nbt));
+                    if (time <= f.cooldown.value){
+                        boolean returned = false;
+                        Inventory  inv = player.getInventory();
+                        ItemStack stack = inv.getItem(38);
+                        if (stack.is(Items.ELYTRA) ) {
+                            for(int j = 0; j < 36; ++j) {
+                                ItemStack stack1 = inv.getItem(j);
+                                if (stack1.is(ItemStack.EMPTY.getItem()) && !returned) {
+                                    stack1 = stack.copy();
+                                    inv.setItem(j, stack1);
+                                    inv.setItem(38,ItemStack.EMPTY);
+                                    returned = true;
+                                }
+                            }
+                            if (!returned) {
+                                player.drop(stack, false);
+                                inv.setItem(38,ItemStack.EMPTY);
+                            }
+                        }
+                        if (f.cooldown.value - time == 0) {
+                            player.sendSystemMessage(Component.literal(f.complte.value),true);
+                        } else {
+                            String msg = f.message.value.replace("%T",String.valueOf(f.cooldown.value-time)).replace("%S",String.valueOf((Math.round((float) (f.cooldown.value - time) / 20))));
+                            player.sendSystemMessage(Component.literal(msg), true);
+                        }
+                    }
+                } catch (NullPointerException ex ){
+                  CompoundTag nbt =new CompoundTag();
+                    nbt.putInt("time_since_combat",f.cooldown.value+1);
+                    player.setComponent(DataComponents.CUSTOM_DATA,CustomData.of(nbt));
+                }
+            }
+        });
+        
+
+    }
+}
